5bc136489433beb6645bfbeaf229f42444127db1
Alex Aredov <aaredov@bk.ru>
2022-06-16 01:34:54 +0300

    hbh

--- hw17_jn_2022/8.py
+++ hw17_jn_2022/8.py
@@ -1,11 +1,9 @@
 def f(x, d, y):
-    if x == y: return d
+    if x == y and d <= 3: return 1
     elif x > y or d > 3: return 10000000
     else:
         if x % 6 == 0:
-            return min(f(x + 2, d + 1, y), f(x * 2, d + 1, y), f(x * 3, d + 1, y))
+            return f(x + 2, d + 1, y) + f(x * 2, d + 1, y) + f(x * 3, d + 1, y)
         else:
-            return min(f(x + 2, d, y), f(x * 2, d, y), f(x * 3, d, y))
-if f(1, 0, 300) <= 3:
-    print(f(1, 0, 300))
-#???
+            return f(x + 2, d, y) + f(x * 2, d, y) + f(x * 3, d, y)
+print(f(1, 0, 300))
