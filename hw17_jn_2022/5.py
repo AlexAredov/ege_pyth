626e47ac89bdf9d117a2cc39bf16bf1e3e16ff4a
Alex Aredov <aaredov@bk.ru>
2022-06-16 00:54:26 +0300

    hvre

---
+++ hw17_jn_2022/5.py
@@ -0,0 +1,8 @@
+import math
+
+
+def f(x, y):
+    if x == y: return 1
+    elif x > y or x == 12: return 0
+    else: return f(x + 1, y) + f(x + 4, y) + f(math.factorial(x + 1), y)
+print(f(2, 24))
---
+++ hw17_jn_2022/6.py
@@ -0,0 +1,9 @@
+def f(x, h, y):
+    if x == y: return 1
+    elif x > y or x == 12 or x == 20: return 0
+    else:
+        if h == 0:
+            return f(x + 1, 0, y) + f(x + 2, 0, y) + f(x * 3, 1, y)
+        else:
+            return f(x + 1, 0, y) + f(x + 2, 0, y)
+print(f(2, 0, 15) * f(15, 0, 30) * f(30, 0, 38))
