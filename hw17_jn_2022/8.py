89e1105c29caa091d8d61856cbdc7313a7a4f300
Alex Aredov <aaredov@bk.ru>
2022-06-17 15:17:26 +0300

    eifie

--- hw17_jn_2022/8.py
+++ hw17_jn_2022/8.py
@@ -1,10 +1,10 @@
 def f(x, d, y):
     if x == y and d <= 3: return 1
-    elif x > y or d > 3: return 10000000
+    elif x > y or d > 3: return 0
     else:
         if x % 6 == 0:
             return f(x + 2, d + 1, y) + f(x * 2, d + 1, y) + f(x * 3, d + 1, y)
         else:
             return f(x + 2, d, y) + f(x * 2, d, y) + f(x * 3, d, y)
 print(f(1, 0, 300))
-#5552350004912?
+#4912
