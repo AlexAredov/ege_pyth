d29f45758ac0e55c839b748df215fc495482e311
Alex Aredov <aaredov@bk.ru>
2022-06-18 19:50:20 +0300

    ушу

--- var19/21.py
+++ var19/21.py
@@ -4,7 +4,7 @@ def f(s1, s2, n):
         else: return False
     else:
         if n >= 4: return False
-        elif n % 2 == 0: return f(s1 + 5, s2, n + 1) or f(s1**2, s2, n + 1) or f(s1, s2 + 5, n + 1) or f(s1, s2**2, n + 1)
+        elif n % 2 == 0: return f(s1 + 5, s2, n + 1) and f(s1**2, s2, n + 1) and f(s1, s2 + 5, n + 1) and f(s1, s2**2, n + 1)
         else: return f(s1 + 5, s2, n + 1) or f(s1**2, s2, n + 1) or f(s1, s2 + 5, n + 1) or f(s1, s2**2, n + 1)
 
 def f1(s1, s2, n):
@@ -13,7 +13,7 @@ def f1(s1, s2, n):
         else: return False
     else:
         if n >= 2: return False
-        elif n % 2 == 0: return f1(s1 + 5, s2, n + 1) or f1(s1**2, s2, n + 1) or f1(s1, s2 + 5, n + 1) or f1(s1, s2**2, n + 1)
+        elif n % 2 == 0: return f1(s1 + 5, s2, n + 1) and f1(s1**2, s2, n + 1) and f1(s1, s2 + 5, n + 1) and f1(s1, s2**2, n + 1)
         else: return f1(s1 + 5, s2, n + 1) or f1(s1**2, s2, n + 1) or f1(s1, s2 + 5, n + 1) or f1(s1, s2**2, n + 1)
 
 for s in range(1, 96):
