bb0fc04bb42327c407d320edb5c700e1d2fa987c
Alex Aredov <aaredov@bk.ru>
2022-06-19 22:51:59 +0300

    dkcnd

--- var_posledni/15.py
+++ var_posledni/15.py
@@ -1,8 +1,9 @@
-for A in range(0, 1000):
+for A in range(1, 10000):
     f = 0
-    for x in range(0, 100):
-        if (((x % 3 == 0) <= (not(x % 5 == 0))) or (x + A >= 90)) == 0:
+    for x in range(1, 10000):
+        if (((x % 3 == 0) <= (x % 5 != 0)) or (x + A >= 90)) == 0:
             f = 1
             break
     if f == 0:
         print(A)
+        break
--- var_posledni/5.py
+++ var_posledni/5.py
@@ -5,3 +5,5 @@ while n > 0:
     s = str(n % 2) + s
     n //= 2
 print(s)
+n = int(input())
+print(int(str(n),2))
