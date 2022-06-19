9d32a52c9f5a25283c551b6d28723bef1ab4813f
Alex Aredov <aaredov@bk.ru>
2022-06-19 17:37:46 +0300

    иарма

--- hw19_jn_2022/4.py
+++ hw19_jn_2022/4.py
@@ -3,14 +3,15 @@ def f(x, y):
     elif x > y or x == 33: return 0
     else:
         k = 0
-        for i in range(x, x + 100):
-            f = 0
+        for i in range(x + 1, x + 1000):
+            f1 = 0
             for j in range(2, int(i**0.5) + 1):
                 if i % j == 0:
-                    f = 1
+                    f1 = 1
                     break
-            if f == 0:
+            if f1 == 0:
                 k = i
                 break
         return f(x + 2, y) + f(k, y)
 print(f(2, 14) * f(14, 45))
+#881
